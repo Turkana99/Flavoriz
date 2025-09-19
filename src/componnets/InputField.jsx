6203705923e561a5c887895cb3412d213c78dc02
Turkana Mammadova <74309514+Turkana99@users.noreply.github.com>
2025-09-19 17:48:25 +0400

    Add formik and yup for login page

--- src/componnets/InputField.jsx
+++ src/componnets/InputField.jsx
@@ -1,13 +1,12 @@
 
 
-function InputField({ type, placeholder, value, onChange }) {
+function InputField({ type, placeholder, className = "", ...props}) {
   return (
     <input
       type={type}
       placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-      className="p-3 rounded-md bg-white/50 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      className={`p-3 rounded-md bg-white/50 focus:outline-none focus:ring-2 focus:ring-blue-500 ${className}`}
+      {...props}
     />
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,23 @@
 import InputField from "../componnets/InputField";
 import Button from "../componnets/Button";
 import { AuthContext } from "../context/AuthContext";
-import { useState, useContext } from "react";
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import * as Yup from "yup";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
+const validationSchema = Yup.object({
+  username: Yup.string()
+    .min(3, "Username must be at least 3 characters long.")
+    .required("Username mustn't be empty."),
+  password: Yup.string()
+    .min(6, "Password must be at least 3 characters long.")
+    .required("Password mustn't be empty."),
+});
 function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
   const { login } = useContext(AuthContext);
 
   const navigate = useNavigate();
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    login(username, password);
-    navigate("/");
-  };
 
   return (
     <div className="relative w-screen h-screen bg-login-background bg-cover bg-center overflow-hidden">
@@ -22,21 +26,42 @@ function Login() {
       <div className="absolute inset-0 flex justify-center items-center">
         <div className="bg-white/20 backdrop-blur-md p-10 rounded-xl w-96 flex flex-col gap-6">
           <h1 className="text-center text-3xl font-bold text-white">Login</h1>
-          <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-            <InputField
-              type="text"
-              placeholder="Enter username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <InputField
-              type="password"
-              placeholder="Enter paswword"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <Button type="submit">Login</Button>
-          </form>
+          <Formik
+            initialValues={{ username: "", password: "" }}
+            validationSchema={validationSchema}
+            onSubmit={(values) => {
+              login(values.username, values.password);
+              navigate("/");
+            }}
+          >
+            {() => (
+              <Form className="flex flex-col gap-4">
+                <Field
+                  as={InputField}
+                  type="text"
+                  name="username"
+                  placeholder="Enter username"
+                />
+                <ErrorMessage
+                  name="username"
+                  component="div"
+                  className="text-red-700 text-sm"
+                />
+                <Field
+                  as={InputField}
+                  type="password"
+                  name="password"
+                  placeholder="Enter password"
+                />
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className="text-red-700 text-sm "
+                />
+                <Button type="submit">Login</Button>
+              </Form>
+            )}
+          </Formik>
         </div>
       </div>
     </div>
